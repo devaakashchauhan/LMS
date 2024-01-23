1fad7f94617341b157887c4bad507d4b5c51bae6
Akaash-27 <123624396+Akaash-27@users.noreply.github.com>
2024-01-23 15:44:04 +0530

    LoginCTA-Section

--- Frontend/lms/src/component/page/1-Home/Home.jsx
+++ Frontend/lms/src/component/page/1-Home/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Slider from 'react-slick'
 import Hero from '../../ui/2-Hero/Hero'
 import Companies from '../../ui/3-Companies/Companies'
 import Courses from '../../ui/4-Courses/Courses'
 import Categories from '../../ui/7-Categories/Categories'
 import Feedback from '../../ui/8-Feedback/Feedback'
+import LoginCTA from '../../ui/9-CTA/LoginCTA'
 
 
 
@@ -16,6 +16,7 @@ const Home = () => {
             <Courses />
             <Categories />
             <Feedback />
+            <LoginCTA />
         </>
     )
 }
