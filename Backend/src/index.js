9e0d4f95745bbb1791f97be9c64702c3925e1537
Akaash-27 <123624396+Akaash-27@users.noreply.github.com>
2024-02-01 20:01:45 +0530

    User-Routes-Setup

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,19 +1,17 @@
 import dotenv from "dotenv";
 import connectDB from "./db/connectdb.js";
+import { app } from "./app.js";
 import express from "express";
 
 dotenv.config({
   path: "./env",
 });
 
-const app = express();
-
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT || 8000),
-      () => {
-        console.log(`Server is runningat port :- ${process.env.PORT}`);
-      };
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is runningat port :- ${process.env.PORT}`);
+    });
   })
   .catch((err) => {
     console.log("MongoDB Connection faild :- ", err);
