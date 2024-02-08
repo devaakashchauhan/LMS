77568bca35d096558bec41f1a45ab9bc7ec10338
Akaash-27 <123624396+Akaash-27@users.noreply.github.com>
2024-02-08 19:42:47 +0530

    Fix-Bug

--- Frontend/src/component/page/13-AdminDashboardPage/AdminDashboardPage.jsx
+++ Frontend/src/component/page/13-AdminDashboardPage/AdminDashboardPage.jsx
@@ -1,8 +1,28 @@
 import ProfileUI from '../../ui/16-Profile/ProfileUI'
 import AdminSidebarUI from '../../ui/15-Sidebar/AdminSidebarUI'
 import Userchk from '../../ui/19-UserChk/Userchk'
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function AdminDashboardPage() {
+    const navigate = useNavigate()
+
+    axios.post('/api/v1/users/userDetails',
+
+    )
+        .then(function ak(response) {
+            // console.log(response);
+            console.log(response);
+
+            const user = response.data.data.role;
+            if (user !== "admin") {
+                navigate("/login")
+            }
+        })
+        .catch(function (error) {
+            // console.log(error);
+
+        });
     return (
         <>
             <Userchk />
--- Frontend/src/component/page/14-TeacherDashboardPage/TeacherDashboardPage.jsx
+++ Frontend/src/component/page/14-TeacherDashboardPage/TeacherDashboardPage.jsx
@@ -1,8 +1,28 @@
 import ProfileUI from '../../ui/16-Profile/ProfileUI'
 import TeacherSidebarUI from '../../ui/15-Sidebar/TeacherSidebarUI'
 import Userchk from '../../ui/19-UserChk/Userchk'
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function TeacherDashboardPage() {
+    const navigate = useNavigate()
+
+    axios.post('/api/v1/users/userDetails',
+
+    )
+        .then(function ak(response) {
+            // console.log(response);
+            console.log(response);
+
+            const user = response.data.data.role;
+            if (user !== "teacher") {
+                navigate("/login")
+            }
+        })
+        .catch(function (error) {
+            // console.log(error);
+            
+        });
     return (
         <>
             <Userchk />
