e5f0cc4d780b59f192eb052e73c27318501890d9
Akaash-27 <123624396+Akaash-27@users.noreply.github.com>
2024-02-14 17:16:47 +0530

    Course-Page-Bug-Fix

--- Frontend/src/component/ui/20-CourseView/CourseVieew.jsx
+++ Frontend/src/component/ui/20-CourseView/CourseVieew.jsx
@@ -27,7 +27,7 @@ function CourseVieew() {
             <div className='w-full bg-[#E9F8F3B2] p-0 maincard'>
                 <div className="text-center">
                     <h1 className='text-3xl py-3 font-bold '>Most Popular <span className='text-[#20B486]'>Courses</span></h1>
-                    <p className='text-[#6D737A]'>Various versions have evolved over the years, sometimes by accident.</p>
+
                 </div>
                 <div className=" md:max-w-[1480px] m-auto grid md:grid-cols-4 max-w-[600px]">
                     {videos.map((video, index) => (
